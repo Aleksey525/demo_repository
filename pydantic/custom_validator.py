81f287a7720c3f72906279ee533270d0ba33e2f9
Aleksey525 <alex_tolchin@mail.ru>
2024-11-27 02:09:56 +0300

    Добавил кастомный валидатор поля

---
+++ pydantic/custom_validator.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, ValidationError, field_validator
+from typing import List
+
+
+class Fruit(BaseModel):
+    name: str
+    color: str
+    weight: float
+
+    @field_validator('weight')
+    def weight_is_positive(cls, v):
+        if v <= 0:
+            raise ValueError('Вес должен быть положительным')
+        return v
+
+
+class Basket(BaseModel):
+    fruits: List[Fruit]
+
+
+try:
+    basket = Basket(
+        fruits = [
+            Fruit(name='apple', color='green', weight=0.2),
+            Fruit(name='banana', color='yellow', weight=-6)
+        ]
+    )
+
+    print(basket)
+
+except ValidationError as error:
+    print(error)
