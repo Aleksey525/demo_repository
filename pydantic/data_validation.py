5df1589588d511ec2a2f134468d241ce4a4d358d
Aleksey525 <alex_tolchin@mail.ru>
2024-11-27 01:07:24 +0300

    Написал вложенную схему валидации данных

---
+++ pydantic/data_validation.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, ValidationError
+from typing import List
+
+
+class Fruit(BaseModel):
+    name: str
+    color: str
+    weight: float
+
+
+class Basket(BaseModel):
+    fruits: List[Fruit]
+
+
+try:
+    basket = Basket(
+        fruits = [
+            Fruit(name='apple', color='green', weight=0.2),
+            Fruit(name='banana', color='yellow', weight='one')
+        ]
+    )
+
+    print(basket)
+
+except ValidationError as error:
+    print(error)
+
+
