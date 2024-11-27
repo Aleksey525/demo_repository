30511c27513bb2b76323d78a3510aeca3108c15f
Aleksey525 <alex_tolchin@mail.ru>
2024-11-27 03:16:36 +0300

    Кастомный валидатор с Annotated

---
+++ pydantic/validator_with_annotated.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, ValidationError, AfterValidator
+from typing import Annotated
+from typing import List
+
+
+def weight_is_positive(v:float) -> float:
+    if v <= 0:
+        raise ValueError('Вес должен быть положительным')
+    return v
+
+
+PositiveFloat = Annotated[float, AfterValidator(weight_is_positive)]
+
+
+class Fruit(BaseModel):
+    name: str
+    color: str
+    weight: PositiveFloat
+
+
+class Basket(BaseModel):
+    fruits: List[Fruit]
+
+
+try:
+    basket = Basket(
+        fruits = [
+            Fruit(name='apple', color='green', weight=0.2),
+            Fruit(name='banana', color='yellow', weight=-6)
+        ]
+    )
+
+    print(basket)
+
+except ValidationError as error:
+    print(error)
