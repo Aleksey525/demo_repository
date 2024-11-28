84e926f3e9b8402c7de8147bf59911e9a20d9648
Aleksey525 <alex_tolchin@mail.ru>
2024-11-28 21:59:02 +0300

    Добавил скрипт для валидации env

---
+++ pydantic/env_validation_script.py
@@ -0,0 +1,71 @@
+from pydantic import BaseModel, Field, ValidationError, field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional
+
+
+class RedisSettings(BaseModel):
+    host: str
+    port: int
+    password: Optional[str] = Field(default=None)
+
+    @field_validator('port')
+    def validate_port(cls, v):
+        if not 0 <= v <= 65535:
+            raise ValueError('Порт должен быть в диапазоне от 0 до 65535')
+        return v
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env_test',
+        extra='ignore',
+        env_nested_delimiter='__'
+    )
+    redis: RedisSettings
+    optional_settings: Optional[str] = Field(default=None)
+
+
+try:
+    settings = Settings()
+    print(settings.model_dump())
+except ValidationError as e:
+    print(f'Ошибки валидации: {e}')
+
+
+
+
+
+
+
+# import os
+#
+# from pydantic import BaseModel, ValidationError
+#
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+#
+#
+# class RedisSettings(BaseModel):
+#     host: str
+#     port: int
+#
+#
+# class Settings(BaseSettings):
+#     model_config = SettingsConfigDict(case_sensitive=True)
+#     redis: RedisSettings
+#
+#
+# os.environ['redis'] = '{"host": "localhost", "port": 6379}'
+#
+#
+# try:
+#     Settings()
+#     print(Settings().model_dump())
+# except ValidationError as e:
+#     print(e)
+
+
+
+
+
+
+
