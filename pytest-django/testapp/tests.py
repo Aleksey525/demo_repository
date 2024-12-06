65bb329cced6975cc03a4ff2b82229463ea402ef
Aleksey525 <alex_tolchin@mail.ru>
2024-12-06 10:33:59 +0300

    Удалил класс из тестов в Джанго приложении

--- pytest-django/testapp/tests.py
+++ pytest-django/testapp/tests.py
@@ -4,19 +4,19 @@ import pytest
 
 
 @pytest.mark.django_db
-class TestUser:
+def test_create_user() -> None:
+    name = 'test_user'
+    email = 'test@test.com'
+    password = 'test_password'
+    user = User.objects.create_user(name, email, password)
+    user.save()
 
-    def test_create_user(self) -> None:
-        name = 'test_user'
-        email = 'test@test.com'
-        password = 'test_password'
-        user = User.objects.create_user(name, email, password)
-        user.save()
+    assert User.objects.filter(username=name).exists()
+    assert User.objects.get(username=name).email == email
+    assert User.objects.get(username=name).check_password(password) is True
 
-        assert User.objects.filter(username=name).exists()
-        assert User.objects.get(username=name).email == email
-        assert User.objects.get(username=name).check_password(password) is True
 
-    def test_check_user_exclusion(self) -> None:
-        with pytest.raises(User.DoesNotExist, match=r'User matching query does not exist'):
-            User.objects.get(username='no_exists_user')
+@pytest.mark.django_db
+def test_check_user_exclusion() -> None:
+    with pytest.raises(User.DoesNotExist, match=r'User matching query does not exist'):
+        User.objects.get(username='no_exists_user')
